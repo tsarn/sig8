5ff11597a03d865b4b2cf6c52eb79ac9da619758
Arnold Tsepov <tsarn@users.noreply.github.com>
2020-07-15 17:49:05 +0300

    Work on sprite editor

--- sig8tk/sig8tk.h
+++ sig8tk/sig8tk.h
@@ -13,3 +13,40 @@
 #define BACKGROUND_COLOR DARK_BLUE
 #define TOOLBAR_COLOR LIGHT_GRAY
 #define ICON_COLOR INDIGO
+
+// Resources
+
+typedef enum {
+    RESOURCE_SPRITE = 1,
+    RESOURCE_TILEMAP,
+    RESOURCE_SFX,
+    RESOURCE_MUSIC,
+    RESOURCE_FONT,
+} ResourceType;
+
+typedef struct {
+    int width;
+    int height;
+    int frames;
+    uint8_t *data;
+} ResourceSprite;
+
+typedef struct {
+    ResourceType type;
+    const char *name;
+    union {
+        ResourceSprite sprite;
+    };
+} Resource;
+
+// Sprite editor
+
+typedef struct {
+    ResourceSprite sprite;
+    int fg, bg;
+} SpriteEditor;
+
+extern SpriteEditor spriteEditor;
+
+void DrawSpriteEditor(void);
+void InitSpriteEditor(void);
