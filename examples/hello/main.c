464a59a3d676748492fd8a8afcc07576a161ef49
Arnold Tsepov <tsarn@users.noreply.github.com>
2020-07-18 23:53:23 +0300

    Change palette!

--- examples/hello/main.c
+++ examples/hello/main.c
@@ -15,10 +15,10 @@ int main()
         ClearScreen(BLACK);
 
         for (int i = 0; i < 12; ++i) {
-            for (int j = 8; j < 16; ++j) {
-                int t1 = t + i * 4 - (j - 8) * 4;
+            for (int j = 0; j < 8; ++j) {
+                int t1 = t + i * 4 - j * 4;
                 int y = 45 - j + cosf(t1 / 10.0f) * 10.0f;
-                RemapColor(WHITE, j);
+                RemapColor(WHITE, j == 7 ? WHITE : j);
                 DrawSprite(36 + i * 8, y, 10 + i,SPRITE_MASK_COLOR(BLACK));
             }
         }
