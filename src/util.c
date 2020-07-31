b20964d724d4e2a2dcdcad0303b5ee55824dc1e2
Arnold Tsepov <tsarn@users.noreply.github.com>
2020-07-31 11:55:39 +0300

    Drop the hex

--- src/util.c
+++ src/util.c
@@ -1,29 +1,5 @@
 #include "sig8_internal.h"
 
-Color ColorFromHex(const char *hex)
-{
-    Color res;
-    assert(hex[0] == '#');
-    int len = strlen(hex);
-
-    unsigned value = strtol(hex + 1, NULL, 16);
-
-    if (len == 4) {
-        res.r = (value >> 8);
-        res.r = (res.r << 4) | res.r;
-        res.g = (value >> 4) & 0xf;
-        res.g = (res.g << 4) | res.g;
-        res.b = value & 0xf;
-        res.b = (res.b << 4) | res.b;
-    } else if (len == 7) {
-        res.r = (value >> 16);
-        res.g = (value >> 8) & 0xff;
-        res.b = value & 0xff;
-    }
-
-    return res;
-}
-
 char *Format(const char *fmt, ...)
 {
     va_list args;
